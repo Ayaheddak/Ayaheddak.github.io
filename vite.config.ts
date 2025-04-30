f9efdd292c761875f0e76ec0903cf662b318f4b4
Ayaheddak <94086031+Ayaheddak@users.noreply.github.com>
2025-05-01 00:47:06 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  base: './', // ✅ this makes file:/// work
+  base: '/', 
   plugins: [react()],
   resolve: {
     alias: {
