7005207fd4511db2b143b43b77dc2f9d14723ec0
Ayaheddak <94086031+Ayaheddak@users.noreply.github.com>
2025-05-01 01:27:33 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
+const isGitHubPages = process.env.GITHUB_PAGES === 'true';
+
 export default defineConfig({
-  base: '/', 
+  base: isGitHubPages ? './' : '/',
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
-  },
-  optimizeDeps: {
-    exclude: ['lucide-react'],
   },
 });
